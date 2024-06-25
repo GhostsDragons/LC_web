3c7b2c7a84250c0a86a74b2fb33b05b46321f81e
Aaryan Bhagat <bhagataaryan2004@gmail.com>
2024-06-25 23:57:57 +0530

    Random Transitions

--- lib/Pages/transition_page.dart
+++ lib/Pages/transition_page.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:lc_web/Pages/home.dart';
 import 'package:lottie/lottie.dart';
@@ -11,17 +12,29 @@ class TransitionPage extends StatefulWidget {
 }
 
 class _TransitionPageState extends State<TransitionPage> {
-  // final String name;
-  // final String grade;
-  // final String board;
+  final List<String> animationUrls = [
+    'https://lottie.host/cd71aca9-d707-466c-92e3-2c78f9995160/BvfvtJ3M7o.json',
+    'https://lottie.host/5b316198-bcc2-430d-bac1-dff2617327e5/DaTRDfIqS2.json',
+    'https://lottie.host/43a51589-4a7e-4da5-8b3f-af0cd4d7b974/N5Ol1voZlf.json',
+  ];
 
-  // _TransitionPageState(this.grade, this.board, this.name);
+  final List<String> randomTexts = [
+    "Learning redefined",
+    "Let's make learning fun !",
+    "Discover new possibilities!",
+  ];
+
+  late String _selectedAnimationUrl;
+  late String _selectedText;
 
   @override
   void initState() {
     super.initState();
+
+    _selectedAnimationUrl = animationUrls[Random().nextInt(animationUrls.length)];
+    _selectedText = randomTexts[Random().nextInt(randomTexts.length)];
+
     Timer(const Duration(seconds: 5), () {
-      // Navigate to the next page after 5 seconds
       Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const Home()));
     });
   }
@@ -36,52 +49,88 @@ class _TransitionPageState extends State<TransitionPage> {
     return LayoutBuilder(
       builder: (context, constraints) {
         if (constraints.maxWidth < 600) {
-          return MobileLayout(constraints: constraints);
+          return MobileLayout(constraints: constraints, animationUrl: _selectedAnimationUrl, text: _selectedText);
         } else {
-          return DesktopLayout(constraints: constraints);
+          return DesktopLayout(constraints: constraints, animationUrl: _selectedAnimationUrl, text: _selectedText);
         }
       },
     );
   }
 }
 
-class DesktopLayout extends StatefulWidget {
+class DesktopLayout extends StatelessWidget {
   final BoxConstraints constraints;
-  const DesktopLayout({super.key, required this.constraints});
+  final String animationUrl;
+  final String text;
+  const DesktopLayout({super.key, required this.constraints, required this.animationUrl, required this.text});
 
-  @override
-  State<DesktopLayout> createState() => _DesktopLayoutState();
-}
-
-class _DesktopLayoutState extends State<DesktopLayout> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: SizedBox(
-          height: MediaQuery.of(context).size.height * 0.4,
-          width: MediaQuery.of(context).size.width * 0.4,
-          child: Lottie.network(
-            'https://lottie.host/cd71aca9-d707-466c-92e3-2c78f9995160/BvfvtJ3M7o.json',
-            fit: BoxFit.contain
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, 
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.4,
+              width: MediaQuery.of(context).size.width * 0.4,
+              child: Lottie.network(
+                animationUrl,
+                fit: BoxFit.contain
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.1,
+              width: MediaQuery.of(context).size.width * 0.2,
+              child: Text(
+                text, 
+                style: const TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
   }
 }
 
-class MobileLayout extends StatefulWidget {
+class MobileLayout extends StatelessWidget {
   final BoxConstraints constraints;
-  const MobileLayout({super.key, required this.constraints});
+  final String animationUrl;
+  final String text;
+  const MobileLayout({super.key, required this.constraints, required this.animationUrl, required this.text});
 
-  @override
-  State<MobileLayout> createState() => _MobileLayoutState();
-}
-
-class _MobileLayoutState extends State<MobileLayout> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, 
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.4,
+              width: MediaQuery.of(context).size.width * 0.4,
+              child: Lottie.network(
+                animationUrl,
+                fit: BoxFit.contain
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+              height: MediaQuery.of(context).size.height * 0.1,
+              width: MediaQuery.of(context).size.width * 0.2,
+              child: Text(
+                text, 
+                style: const TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
