413fdb1b339ecd2000458a51f3787a2cd9e6ded7
GhostsDragons <harigoyal2003@gmail.com>
2024-07-22 18:37:21 +0530

    User Onboarding

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: unused_import
 
 import 'package:flutter/material.dart';
+import 'package:lc_web/Firebase/_auth.dart';
 import 'package:lc_web/Pages/course_catalog.dart';
 import 'package:lc_web/Pages/home.dart';
 // import 'package:lc_web/Pages/settings.dart';
@@ -31,6 +32,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var user = Auth().getUser();
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Learners\' Club',
@@ -38,53 +40,8 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
 
-      home: const AuthWrapper(),
+      home:(user != null)? const Home(): const Login(),
 
-      // initialRoute: '/login',
-      //
-      // routes: {
-      //   '/login': (context) => const Login(),
-      //   '/signup': (context) => const Signup(),
-      //   // '/home': (context) => const Home(),
-      //   // '/settings': (context) => const Settings(),
-      //   // '/transition': (context) => const TransitionPage(),
-      //   // '/onboarding': (context) => const Onboarding(),
-      //   // '/transition': (context) => const TransitionPage(),
-      //   // '/onboarding': (context) => const Onboarding(),
-      //   // '/welcome' : (context) => const WelcomePageOne(),
-      //   // '/profile' : (context) => const Profile(),
-      //   // '/trial': (context) => const Trial(),
-      // },
-    );
-  }
-}
-
-class AuthWrapper extends StatelessWidget {
-  const AuthWrapper({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<User?>(
-      stream: FirebaseAuth.instance.authStateChanges(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Scaffold(
-            body: Center(
-              child: SizedBox(
-                height: MediaQuery.of(context).size.height * 0.4,
-                width: MediaQuery.of(context).size.width * 0.4,
-                child: Lottie.network(
-                    'https://lottie.host/ae9cc085-4db3-49e6-8aa6-47e89cd693ad/ULVww8ZhqK.json',
-                    fit: BoxFit.contain),
-              ),
-            ),
-          );
-        } else if (snapshot.hasData) {
-          return const Home();
-        } else {
-          return const Login();
-        }
-      },
     );
   }
 }
