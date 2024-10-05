001f7b3ddb16a61717271b7cc1f285bf79fc1a0b
GhostDragons <harigoyal2003@gmail.com>
2024-10-05 18:47:15 +0530

    UpdateProfilePir returns URL

--- lib/Firebase/_storage.dart
+++ lib/Firebase/_storage.dart
@@ -3,35 +3,33 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:lc_web/Firebase/_auth.dart';
 
-class Storage{
+class Storage {
   final user = Auth().getUser();
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final FirebaseStorage _storage = FirebaseStorage.instance;
 
-  Future<void> updateProfilePic(String folder, XFile? file) async{
-
+  Future<String> updateProfilePic(String folder, XFile? file) async {
     try {
       if (file != null) {
         Blob blob = Blob(await file.readAsBytes());
         final imagePath = "$folder/${user!.uid}/profilePicture.jpg";
-        print(imagePath);
         Reference imageRef = _storage.ref().child(imagePath);
 
         await imageRef.putBlob(blob.bytes);
 
         final downloadURL = await imageRef.getDownloadURL();
-        user!.updatePhotoURL(downloadURL);
-        updateDatabase('users', {'profilePictureURL': downloadURL});
+        return downloadURL;
       } else {
-        return;
+        return ("");
       }
     } catch (e) {
       // TODO: Error handling
       print(e);
     }
+    return ("");
   }
 
-  Future<void> updateDatabase(String  collection, Map data) async{
+  Future<void> updateDatabase(String collection, Map data) async {
     data.forEach((key, value) {
       _firestore.collection(collection).doc(user!.uid).update({
         key: value,
